d21846bebd4d7f19288e314d9f434336e507285e
Russel Winder <russel@winder.org.uk>
2020-04-24 14:44:15 +0100

    source/main.d only included in application build.

--- source/main.d
+++ source/main.d
@@ -1,6 +1,6 @@
 //  GFontBrowser — A font browser for GTK+, Fontconfig, Pango based systems.
 //
-//  Copyright © 2013–2014, 2017–2019  Russel Winder <russel@winder.org.uk>
+//  Copyright © 2013–2014, 2017–2020  Russel Winder <russel@winder.org.uk>
 //
 //  This program is free software: you can redistribute it and/or modify it under the terms of the GNU
 //  General Public License as published by the Free Software Foundation, either version 3 of the License, or
@@ -34,36 +34,31 @@ import applicationWindow: getApplicationWindow;
 import configuration: applicationName, versionNumber;
 import fontCatalogue: initialise;
 
-version (unittest) {
-    @("Gratuitous test to ensure there is at least one test") unittest {
-        assert(0 == 0);
-    }
+int main(string[] args) {
+    //auto application = new Application("uk.org.winder.gfrontbrowser", GApplicationFlags.HANDLES_COMMAND_LINE);
+    auto application = new Application("uk.org.winder.gfrontbrowser", GApplicationFlags.FLAGS_NONE);
+    Util.setApplicationName(applicationName);
+    application.addMainOption("version", 'v', GOptionFlags.NONE, GOptionArg.NONE, "Show the " ~ applicationName ~ " version.", null);
+    application.addOnStartup(delegate void(GioApplication app) {
+        initialise();
+        auto applicationWindow = getApplicationWindow(cast(Application) app);
+    });
+    application.addOnActivate(delegate void(GioApplication app) {
+    });
+    application.addOnHandleLocalOptions(delegate int(VariantDict vd, GioApplication a){
+        if (vd.contains("version")) {
+            writeln(versionNumber);
+            return 0;
+        }
+        return -1;
+    });
+    application.addOnCommandLine(delegate int(ApplicationCommandLine acl, GioApplication a){
+        return -1;
+    });
+    return application.run(args);
 }
-else {
-    int main(string[] args) {
-        //auto application = new Application("uk.org.winder.gfrontbrowser", GApplicationFlags.HANDLES_COMMAND_LINE);
-        auto application = new Application("uk.org.winder.gfrontbrowser", GApplicationFlags.FLAGS_NONE);
-        Util.setApplicationName(applicationName);
-        application.addMainOption("version", 'v', GOptionFlags.NONE, GOptionArg.NONE, "Show the " ~ applicationName ~ " version.", null);
-        application.addOnStartup(delegate void(GioApplication app) {
-                initialise();
-                auto applicationWindow = getApplicationWindow(cast(Application) app);
-        });
-        application.addOnActivate(delegate void(GioApplication app) {
-        });
-        application.addOnHandleLocalOptions(delegate int(VariantDict vd, GioApplication a){
-            if (vd.contains("version")) {
-                writeln(versionNumber);
-                return 0;
-            }
-            return -1;
-        });
-        application.addOnCommandLine(delegate int(ApplicationCommandLine acl, GioApplication a){
-            return -1;
-        });
-        return application.run(args);
-    }
-}
+
+// No unit tests in this file.
 
 //  Local Variables:
 //  mode: d
